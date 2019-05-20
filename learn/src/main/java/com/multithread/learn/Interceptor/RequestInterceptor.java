4b666379382b71cfde1699b511da6cc1a5f9f8f8
yangshilei <17751693884@163.com>
2019-05-20 15:49:16 +0800

    拦截器代码补充

--- learn/src/main/java/com/multithread/learn/Interceptor/RequestInterceptor.java
+++ learn/src/main/java/com/multithread/learn/Interceptor/RequestInterceptor.java
@@ -1,7 +1,11 @@
 package com.multithread.learn.Interceptor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Description：请求url拦截器
@@ -11,4 +15,14 @@ import org.springframework.web.servlet.HandlerInterceptor;
  */
 @Slf4j
 public class RequestInterceptor implements HandlerInterceptor {
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    String requestURI = request.getRequestURI();
+    if(requestURI.startsWith("/test")){
+      log.debug("鉴权失败，无权限的请求路径！");
+      return true;
+    }
+    return false;
+  }
 }
