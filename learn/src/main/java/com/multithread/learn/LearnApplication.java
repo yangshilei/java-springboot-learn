a93d67ad3cce97abcd4b8b272372e571e95865d5
yangshilei <17751693884@163.com>
2020-03-16 19:52:53 +0800

    定时任务代码提交

--- learn/src/main/java/com/multithread/learn/LearnApplication.java
+++ learn/src/main/java/com/multithread/learn/LearnApplication.java
@@ -3,6 +3,7 @@ package com.multithread.learn;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
