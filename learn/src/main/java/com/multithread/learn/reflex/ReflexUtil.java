86ae821ced150d434e083b989a62a493e411bf9b
LX_yangshilei <13222031857@189.cn>
2020-06-02 23:37:53 +0800

    fadfasd

--- learn/src/main/java/com/multithread/learn/reflex/ReflexUtil.java
+++ learn/src/main/java/com/multithread/learn/reflex/ReflexUtil.java
@@ -2,10 +2,10 @@ package com.multithread.learn.reflex;
 
 public class ReflexUtil {
     public static void main(String[] args) throws ClassNotFoundException {
-        System.out.println(123);
+        System.out.println(1443);
         Class<?> aClass = Class.forName("com.multithread.learn.pojo.UserReq");
         String simpleName = aClass.getSimpleName();
         System.out.println(simpleName);
-        System.out.println(133);
+        System.out.println(1343);
     }
 }
